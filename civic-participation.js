b5f1a69d9e39751020ca6ee4c06119f729f7d6c7
star-one <simon@star-one.org.uk>
2014-03-31 03:57:45 +0100

    UI changes to try to placate Apple plus moving some Advices from Civic participation to Acting under Concern

--- civic-participation.js
+++ civic-participation.js
@@ -5,15 +5,9 @@ var strings = [
 
 'Do you exercise your civic responsibilities and support acceptable legislation? Do you pray for those in authority? Are you careful to avoid defrauding the public treasury? In or out of court do you affirm instead of taking oaths?<br /><em>Great Plains Yearly Meeting, Advices and Queries, Truth</em>',
 
-'We value our part in shaping the laws of our country. Our task is to see that laws serve God\'s purposes and build a just social order. Our first allegiance should be to God, and if this conflicts with any compulsion of the state, we serve our country best by remaining true to our higher loyalty.<br /><em>Pacific Yearly Meeting, Advices and Queries, Social and Civic Responsibility, Advices</em>',
-
-'If, by divine leading, our attention is focused on a law that is contrary to God\'s law, we must proceed with care. Before acting, Friends should pray for further guidance and speak with the Meeting, family members, and all those who might be affected by the decision. If a decision involves disobedience to the law, we should make the grounds of our action clear to all concerned and be prepared to suffer any penalties without evasion. As a community, we must care for those who suffer for conscience\'s sake.<br /><em>Pacific Yearly Meeting, Advices and Queries, Social and Civic Responsibility, Advices</em>',
-
 'What am I doing to carry my share of responsibility for the government of our community, nation, and world? Am I persistent in my efforts to promote constructive change?<br /><em>Pacific Yearly Meeting, Advices and Queries, Social and Civic Responsibility, Queries for Individuals</em>',
 
 'Remember your responsibility as citizens of Aotearoa for the government of our country and for its relations with other countries, particularly our neighbours in the South Pacific. How can we help our nation to promote international peace, justice and care for the earth?<br /><em>Aotearoa/New Zealand Yearly Meeting, Advices and Queries, E10</em>',
-
-'Obey the laws of the state, except when they conflict with your inner conviction. Work to amend laws that you consider unjust. If you feel called to civil disobedience, seek the guidance and support of your Meeting. Be prepared to accept the consequences cheerfully.<br /><em>Aotearoa/New Zealand Yearly Meeting, Advices and Queries, E4</em>',
 
 'All those who commit crimes have that of God within them, as do those who are the victims of crime. Do you recognise or share in the work that is being done towards better conditions and rehabilitation for prisoners, towards alternatives to imprisonment, and towards meeting the needs of victims of crime?<br /><em>Aotearoa/New Zealand Yearly Meeting, Advices and Queries, E5</em>',
 
--- concern.js
+++ concern.js
@@ -1,11 +1,15 @@
 // Acting under Concern
 function updateConcern() {
 var strings = [
+'Obey the laws of the state, except when they conflict with your inner conviction. Work to amend laws that you consider unjust. If you feel called to civil disobedience, seek the guidance and support of your Meeting. Be prepared to accept the consequences cheerfully.<br /><em>Aotearoa/New Zealand Yearly Meeting, Advices and Queries, E4</em>',
+
 'Respect the laws of the state but let your first loyalty be to God\'s purposes. If you feel impelled by strong conviction to break the law, search your conscience deeply. Ask your meeting for the prayerful support which will give you strength as a right way becomes clear.<br /><em>Britain Yearly Meeting, Advices and Queries, number 35</em>',
 
 'Do you uphold those who are acting under concern, even if their way is not yours? Can you lay aside your own wishes and prejudices while seeking with others to find God\'s will for them?<br /><em>Britain Yearly Meeting, Advices and Queries, number 36</em>',
 
+'We value our part in shaping the laws of our country. Our task is to see that laws serve God\'s purposes and build a just social order. Our first allegiance should be to God, and if this conflicts with any compulsion of the state, we serve our country best by remaining true to our higher loyalty.<br /><em>Pacific Yearly Meeting, Advices and Queries, Social and Civic Responsibility, Advices</em>',
 
+'If, by divine leading, our attention is focused on a law that is contrary to God\'s law, we must proceed with care. Before acting, Friends should pray for further guidance and speak with the Meeting, family members, and all those who might be affected by the decision. If a decision involves disobedience to the law, we should make the grounds of our action clear to all concerned and be prepared to suffer any penalties without evasion. As a community, we must care for those who suffer for conscience\'s sake.<br /><em>Pacific Yearly Meeting, Advices and Queries, Social and Civic Responsibility, Advices</em>',
 ];
 
 var randomIndex = Math.floor(Math.random() * strings.length);
